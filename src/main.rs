72a47760b2c0f1c2c4f84033875fe91c81a8c06a
KLohger <fanimdude32@gmail.com>
2023-11-09 15:03:48 +0100

    cleaned up

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,46 @@ use glium::{
 };
 use glium::{uniforms, IndexBuffer, Program as ShaderProgram};
 use winit::event_loop::EventLoopBuilder;
+use winit::window::WindowBuilder;
 #[derive(Clone, Copy)]
 pub struct Vertex {
     pub position: [f32; 3],
 }
 implement_vertex!(Vertex, position);
 pub struct Mesh {
-    pub vertices: VertexBuffer<Vertex>,
-    pub indices: IndexBuffer<u32>,
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u32>,
+    pub primitive_type: PrimitiveType,
 }
 
-fn cam_shells<F: Facade>(facade: &F, shell_resolutions: Vec<[usize; 2]>) -> Vec<Mesh> {
+impl Mesh {
+    pub fn vertex_buffer<F: Facade>(
+        &self,
+        facade: &F,
+    ) -> Result<VertexBuffer<Vertex>, glium::vertex::BufferCreationError> {
+        VertexBuffer::immutable(facade, &self.vertices)
+    }
+    pub fn index_buffer<F: Facade>(
+        &self,
+        facade: &F,
+    ) -> Result<IndexBuffer<u32>, glium::index::BufferCreationError> {
+        IndexBuffer::immutable(facade, self.primitive_type, &self.indices)
+    }
+}
+
+fn cam_shells(shell_resolutions: Vec<[usize; 2]>) -> Vec<Mesh> {
     let mut vec = Vec::with_capacity(shell_resolutions.len());
-    for (shell_index, shell_resolution) in shell_resolutions.into_iter().enumerate() {
+    for (shell_index, shell_resolution) in shell_resolutions
+        .into_iter()
+        .enumerate()
+        .map(|(shell_index, shell_resolution)| (shell_index as f32, shell_resolution))
+    {
         vec.push(Mesh {
-            vertices: VertexBuffer::immutable(facade, {
+            vertices: {
                 // fill vec with vertices
                 let mut vertices = vec![
                     Vertex {
-                        position: [0.0, 0.0, 0.0]
+                        position: [0.0, 0.0, shell_index]
                     };
                     shell_resolution[0] * shell_resolution[1]
                 ];
@@ -32,27 +53,22 @@ fn cam_shells<F: Facade>(facade: &F, shell_resolutions: Vec<[usize; 2]>) -> Vec<
                     .enumerate()
                     .for_each(|(vertex_index, vertex)| {});
 
-                &vertices
-            })
-            .unwrap(),
-            indices: IndexBuffer::new(facade, PrimitiveType::TrianglesList, {
+                vertices
+            },
+            indices: {
                 // fill vec with indices
                 let indices = vec![0; shell_resolution[0] * shell_resolution[1]];
 
-                &indices
-            })
-            .unwrap(),
+                indices
+            },
+            primitive_type: PrimitiveType::TrianglesList,
         })
     }
 
     return vec;
 }
 
-fn gen_plane() -> Mesh {
-    todo!()
-}
-
-const vertex_shader_src: &str = r#"
+const VERTEX_SHADER_SRC: &str = r#"
     ##version 140
 
     in vec3 position;
@@ -61,7 +77,7 @@ const vertex_shader_src: &str = r#"
         gl_Position = vec4(position, 1.0);
     }
 "#;
-const fragment_shader_src: &str = r#"
+const FRAGMENT_SHADER_SRC: &str = r#"
     ##version 140
 
     out vec4 color;
@@ -74,28 +90,39 @@ const fragment_shader_src: &str = r#"
 fn main() {
     let event_loop = EventLoopBuilder::new().build();
     let (window, display) = SimpleWindowBuilder::new()
-        .with_title("shell texture swag wow™")
+        .set_window_builder(
+            WindowBuilder::new()
+                .with_title("shell texture swag wow™")
+                .with_visible(false),
+        )
         .build(&event_loop);
     let cam_shells = cam_shells(
-        &display,
         (1..=16)
             .rev()
             .map(|res| res * res)
             .map(|res| [res, res])
             .collect::<Vec<_>>(),
-    );
+    )
+    .into_iter()
+    .map(|mesh| {
+        (
+            mesh.vertex_buffer(&display).unwrap(),
+            mesh.index_buffer(&display).unwrap(),
+        )
+    })
+    .collect::<Vec<_>>();
     let cam_shell_program =
-        ShaderProgram::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+        ShaderProgram::from_source(&display, VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC, None).unwrap();
 
     event_loop.run(move |ev, _, control_flow| match ev {
         winit::event::Event::RedrawRequested(window_id) if window.id() == window_id => {
             let mut frame = display.draw();
             frame.clear_color(0.0, 0.0, 0.0, 1.0);
-            for cam_shell in &cam_shells {
+            for (vertex_buffer, index_buffer) in &cam_shells {
                 frame
                     .draw(
-                        &cam_shell.vertices,
-                        &cam_shell.indices,
+                        vertex_buffer,
+                        index_buffer,
                         &cam_shell_program,
                         &uniforms::EmptyUniforms,
                         &Default::default(),
